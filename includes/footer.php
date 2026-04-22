eaa4367b80c0cac00cbe870ced7008ba5f009c17
hiba hitana <hibahtn17@gmail.com>
2026-04-22 15:52:09 +0100

    supprimer les dossier extra

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<hr>
-<footer>
-    <p>&copy; 2026 Nvalidi Project</p>
-</footer>
-<script src="../assets/js/script.js"></script>
-</body>
-</html>
--- includes/header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Nvalidi</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
-<body>
